667a07a2530aaa548b8ad5fbdabbed9c0db18ebf
David Stringer <David.Stringer@researchresearch.com>
2016-02-05 17:07:56 +0000

    start trying to puppet dyn_failover

---
+++ environments/production/modules/localconfig/manifests/dynfailover.pp
@@ -0,0 +1,21 @@
+class dynfailover {
+
+    $dynamichost = hiera('dynamichost')
+    $dynapiuser  = hiera('dynapiuser')
+    $dynapipass  = hiera('dynapipass')
+
+    file ( 'dyn_failover':
+        path    => "/usr/local/bin/dyn_failover",
+        mode    => 0755,
+        content => template('localconfig/dyn_failover.erb')
+    }
+
+    cron ( 'dynfailover':
+        ensure  => present,
+        command => "/usr/local/bin/dyn_failover",
+        user    => 'root',
+        target  => 'root',
+        minute  => '*/5'
+    }
+}
+
---
+++ environments/production/modules/localconfig/manifests/externalips.pp
@@ -0,0 +1,9 @@
+class localconfig::externalips {
+
+  ## collect external IP from all other mahcines in cluster
+  Externalip <<| |>>
+
+  ## and export the externalip entry of this resource so it can be acquired by all other nodes
+  @@externalip { "$::clientcert": ip => $::ipaddress_eth0 }
+
+}
