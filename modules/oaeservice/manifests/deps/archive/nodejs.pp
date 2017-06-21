210463b1606f8c3d72d1a6963afbe58ce1290313
David Stringer <David.Stringer@researchresearch.com>
2017-06-21 16:10:38 +0100

    fixed stupid err

--- modules/oaeservice/manifests/deps/archive/nodejs.pp
+++ modules/oaeservice/manifests/deps/archive/nodejs.pp
@@ -3,8 +3,8 @@ class oaeservice::deps::archive::nodejs {
     # Download and unpack the archive
     archive { "node-v6.10.0":
         ensure          => present,
-        url             => $package_url 'https://nodejs.org/dist/v6.10.0/node-v6.10.0-linux-x64.tar.gz',
-        target          => $app_root_dir '/usr/local/node-v6.10.0',
+        url             => 'https://nodejs.org/dist/v6.10.0/node-v6.10.0-linux-x64.tar.gz',
+        target          => '/usr/local/node-v6.10.0',
 #        digest_url      => $checksum_url,
 #        digest_type     => $checksum_type,
         extension       => 'tar.gz',
